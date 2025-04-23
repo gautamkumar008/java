2b45c7bb57bfd01662fd8389b9c71116ab71704e
gautamkumar <mrgautamkumar088@gmail.com>
2025-04-23 11:37:14 +0530

    change

--- Sortingarrs.java
+++ Sortingarrs.java
@@ -9,16 +9,17 @@ public class Sortingarrs {
         System.out.println();
     }
 
+    //code is ok
     public static void main(String[] args) {
-        int arr1[] = { 78 , 52 , 45 , 10 , 99 , 1 };
+        int arr1[] = {78, 52, 45, 10, 99, 1};
         //bubble shorting
         for (int i = 0; i < arr1.length - 1; i++) {
-            for (int j = 0; j < arr1.length-i-1; j++) {
-                if(arr1[j] > arr1[j+1]){
+            for (int j = 0; j < arr1.length - i - 1; j++) {
+                if (arr1[j] > arr1[j + 1]) {
                     //swaping a numbers
                     int temp = arr1[j];
-                    arr1[j] = arr1[j+1];
-                    arr1[j+1] = temp;
+                    arr1[j] = arr1[j + 1];
+                    arr1[j + 1] = temp;
                 }
             }
         }
